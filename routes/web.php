a20b9346fbea999fa214a07846b584b25f125614
Roei Boribor <roeiboribor@gmail.com>
2024-04-07 09:10:24 +0800

    change the home page of laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inspiring::quote();
 });
